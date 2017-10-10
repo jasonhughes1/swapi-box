58c1b355e2a6e8523392a306ab659064b29f10b3
Sam Singer <samuel.singer1@gmail.com>
2017-10-09 21:34:50 -0600

    skeleton of our dumb components are present

---
+++ src/Components/FavoriteButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FavoriteButton = () => {
+  return (
+    <button>
+      FavoriteButton
+    </button>
+  )
+}
+
+export default FavoriteButton;
---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FavoriteButton from './FavoriteButton';
+
+const Header = () => {
+  return (
+    <div>
+      Header
+      <FavoriteButton/>
+    </div>
+  )
+}
+
+export default Header;
