03e90b913bd7db61a9957c18e8e83b0e729a2080
Jason Hughes <jasonhughes088@gmail.com>
2017-10-10 17:28:52 -0600

    scroll component working

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Main from './Main';
 import '../scss/App.css';
+import HomePage from './HomePage';
 
 class App extends Component {
   constructor() {
--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ButtonContainer from './ButtonContainer';
+import Scroller from './Scroller';
 
 const HomePage = () => {
   return (
     <div>
       <ButtonContainer />
+      <Scroller />
     </div>
   )
 }
