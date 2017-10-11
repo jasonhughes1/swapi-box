9e5d55e527778042e672f0148b9075ced053cedf
Sam Singer <samuel.singer1@gmail.com>
2017-10-11 00:00:45 -0600

    scroll is having issues

--- src/Components/App.js
+++ src/Components/App.js
@@ -6,40 +6,83 @@ import '../scss/App.css';
 
 class App extends Component {
   constructor() {
-  super();
+    super();
 
-  this.state = {
-    starWarsData: null
+    this.state = {
+      data: null
+    }
   }
-  this.retrieveData()
-}
   //     filmData: null,
   //     peopleData: null,
   //     planetData: null,
   //     vehicleData: null,
   //     favoriteCards: [],
 
-  retrieveData() {
+  componentDidMount() {
     const films = fetch('https://swapi.co/api/films/').then(data => data.json())
     const people = fetch('https://swapi.co/api/people/').then(data => data.json())
     const planets = fetch('https://swapi.co/api/planets/').then(data => data.json())
     const vehicles = fetch('https://swapi.co/api/vehicles/').then(data => data.json())
-    console.log(planets);
 
     return Promise.all([films, people, planets, vehicles])
-      .then((data) => {
-        this.setState({starWarsData: data})
+    .then((data) => {
+      this.fetchHomeworld(data[1].results)
+      this.fetchSpecies(data[1].results)
+      this.fetchResidents(data[2].results)
+      this.setState({data: data})
+    }).catch((error) => {console.log(error)})
+  }
+
+  fetchHomeworld(data) {
+    const specificHomeworldData = data.map((world, i) => {
+      return fetch(world.homeworld)
+      .then(res => res.json())
+    })
+
+    return Promise.all(specificHomeworldData).then( homeworlds => {
+      homeworlds.map((homeworld, i) => {
+        Object.assign(data[i], {Homeworld: homeworld.name, Population: homeworld.population})
       })
-      .catch((e) => {console.log(e)})
+    })
+  }
+
+  fetchSpecies(data) {
+    const specificSpeciesData = data.map((species, i) => {
+      return fetch(species.species)
+      .then(res => res.json())
+    })
+
+    return Promise.all(specificSpeciesData).then( species => {
+      species.map((specie, i) => {
+        Object.assign(data[i], {Species: specie.name})
+      })
+    })
+  }
+
+  fetchResidents(data) {
+    const specificResidentsData = data.map( (residents, i) => {
+      const newArray = [];
+
+      const specificResidents = residents.residents.map((link, i) => {
+        return fetch(link)
+        .then(res => res.json())
+      })
+      return Promise.all(specificResidents).then( people => {
+        people.map((person, i) => {
+          newArray.push(person.name)
+          Object.assign(residents, {Residents: newArray})
+        })
+      })
+    })
   }
 
   render() {
     const { data } = this.state
-
+    console.log({data});
     if(data) {
       return (
         <div className="App">
-          <Scroller data={this.state.starWarsData[0].results} />
+          <Scroller data={this.state.data[0].results} />
           <Main />
           <HomePage />
         </div>
