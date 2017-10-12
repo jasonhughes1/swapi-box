78ac4dd368e82c17ec69edef7dadf964d679ea64
Sam Singer <samuel.singer1@gmail.com>
2017-10-11 19:00:43 -0600

    you can now toggle between the favorites button and the people button

--- src/Components/Scroll/Scroll.js
+++ src/Components/Scroll/Scroll.js
@@ -2,17 +2,18 @@ import React from 'react';
 import './Scroll.css';
 import Button from '../Button/Button';
 
-const Scroll = ({ data, opening, btnFn, toggleActive }) => {
+const Scroll = ({ data, opening, btnFn, toggleActive, numFav }) => {
   const text = data[opening].Opening
   const filmTitle = data[opening].Title
   const releaseDate = data[opening].Release
+  console.log(numFav);
 
   return (
     <div>
       <div className='crawl-container'>
         <div className='fade'>
           <h1 className='title'>SWAPI-BOX</h1>
-          <Button className={' button favorite'} toggleActive={toggleActive} buttonText='View Favorites' btnFn={btnFn} />
+          <Button className={' button favorite'} toggleActive={toggleActive} buttonText={'View Favorites ' +   numFav} btnFn={btnFn} />
         </div>
         <div className='star-wars'>
           <div className='crawl'>
